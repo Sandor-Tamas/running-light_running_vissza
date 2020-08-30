ccac88bf9413c36c6e6eff501b1437a0e2be63ca
Sandor-Tamas <70057013+Sandor-Tamas@users.noreply.github.com>
2020-08-30 21:12:13 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
-let LED_X = 4
-let LED_Y = 4
+let LED_X = 0
+let LED_Y = 0
 basic.forever(function () {
     led.plot(LED_X, LED_Y)
     basic.pause(500)
     led.unplot(LED_X, LED_Y)
-    if (LED_X == 0) {
-        LED_X = 4
-        if (LED_Y == 0) {
-            LED_Y = 4
+    if (LED_X == 4) {
+        LED_X = 0
+        if (LED_Y == 4) {
+            LED_Y = 0
         } else {
-            LED_Y += -1
+            LED_Y += 1
         }
     } else {
-        LED_X += -1
+        LED_X += 1
     }
 })
